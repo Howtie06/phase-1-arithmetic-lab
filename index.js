6a9e39e57b5ee953d1e98ff02363ae4da8a03cc6
audrey cherop <audreycherop06@gmail.com>
2024-03-20 14:53:20 +0300

    const

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1=31
+const num2=2
+
+const adam = function (num1,num2){
+    return num1*num2
+}
+let multiply = adam(31,2);
+//starting no. 2
+const random = Math.ceil (Math.random ()*6) + 1;
+//starting no.3
+const num3 = 10
+const num4 = 6
+const mod = num3 % num4;
+//start no.4
+const max = Math.max(5,10,15,20)
